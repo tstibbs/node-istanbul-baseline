a97e945eb337f3ccee3082730cd9ec135e8abaab
tstibbs <tstibbs@users.noreply.github.com>
2016-07-21 20:42:18 +0100

    Fixed bug where globbed file were being loaded by mocha too - now needs config in package.json

--- cli.js
+++ cli.js
@@ -1,8 +1,17 @@
 var cover = require('./index.js')
 var path = require('path')
+var fs = require('fs');
 
-var file = process.argv[3];
+var packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+if (packageJson.config == undefined || packageJson.config.istanbul_baseline == undefined || packageJson.config.istanbul_baseline.path == undefined) {
+	throw 'Usage: the calling module\'s package.json should contain a section like this:\n    config: {\n        istanbul_baseline: {\n            path: "./js/*"\n        }\n    }';
+}
+var file = packageJson.config.istanbul_baseline.path;
 
 var absolutePath = path.resolve('.', file);
 
-cover(absolutePath);
+describe('dummy test to load everything', function() {
+	it('should require every file', function() {
+		cover(absolutePath);
+	});
+});
--- index.js
+++ index.js
@@ -8,22 +8,22 @@ function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-function list(path1) {
-	console.log("Glob: " + path1)
-	//TODO get rid of glob, it's nothing but trouble
-	glob.sync(path1).forEach(function(path) {
-		console.log("???: " + path);
-		if (path1 != path) {
+function list(basePath) {
+	glob.sync(basePath).forEach(function(path) {
+		if (basePath != path) {
 			if (endsWith(path, ".js")) {
-				console.log("REQ: " + path);
-				require(path);
+				console.log("Found: " + path);
+				try {
+					require(path);//the file itself might error, but we don't mind here
+				} catch (err) {
+					console.log(err);
+				}
 			} else {
-				console.log("REC: " + path);
 				list(path);
 			}
-			console.log(" ");
 		}
 	});
 }
+
 
 module.exports = list
